12d560d7f8472de96066cb25f49455f6b46b8858
Chathumina Kaushal <chathuminakaushal@gmail.com>
2024-04-03 20:20:25 +0530

    Public and Src

--- client/src/components/Teacher Management/src/App.jsx
+++ client/src/components/Teacher Management/src/App.jsx
@@ -1,26 +1,38 @@
 //import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from "react";
+import React from "react";
+import {BrowserRouter as Router, Route, Routes, Redirect} from "react-router-dom";
 
-//functions
-import {getTest} from "./functions/test";
+//components
+import SideNavbar from './component/SideNavbar';
+import THome from "./pages/THome";
+import TCreate from './pages/TCreate';
+import TLogin from './pages/TLogin';
+import TDetails from './pages/TDetails';
+import THeader from './component/THeader';
+import Tpagetest from './pages/Tpagetest';
+import TUpdate from './pages/TUpdate';
 
-function App() {
-  const [data, setData] = useState("Hello World!");
-
-  useEffect(() => {
-    getTest()
-      .then((res) => {
-        setData(res.message);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1> {data} </h1>
-    </div>
-  );
+//routes
+const App = () =>{
+  return(
+    
+    <Router> 
+      
+      <THeader />
+      <SideNavbar >
+      <Routes>
+        <Route exact path="/" element={<THome />} />
+        <Route exact path="tCreate"  element={<TCreate/>}/>
+        <Route exact path="/tLogin"  element={<TLogin/>}/>
+        <Route exact path="/tDetails" element={<TDetails/>}/>
+        <Route exact path="/tpagetest" element={<Tpagetest/>}/>  
+        <Route path="/tUpdate/:id" element={<TUpdate />} />
+      </Routes>
+      </SideNavbar>
+    </Router>
+  )
 }
 
 export default App;
